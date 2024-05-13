51b94340b0763312f1a39b19b0c900889c136e78
Naïm <naimoudinesaidi@gmail.com>
2024-05-13 16:59:16 +0200

    creation fichier db et ajout d'images

---
+++ client/src/db.js
@@ -0,0 +1,75 @@
+const destinations = [
+  {
+    id: 1,
+    name: "Bodrum",
+    description: "Bodrum, nestled along the sparkling Aegean coast of Turkey, is a picturesque paradise blending ancient charm with modern elegance. Its iconic white-washed buildings cascade down hillsides overlooking azure waters, creating a scene of timeless beauty. The imposing Bodrum Castle, with its medieval towers and ramparts, stands as a testament to the city's rich history. Quaint streets lined with charming cafes and boutique shops invite leisurely strolls, while vibrant marinas host luxury yachts and traditional wooden gulets.",
+    budget: [86,92],
+    img:"/src/assets/images/cities/bodrum.avif"
+  },
+  {
+    id: 2,
+    name: "Paris",
+    description: "Paris, the capital city of France, is renowned worldwide for its rich history, iconic landmarks, and vibrant culture. Situated along the banks of the Seine River, Paris is often called the 'City of Light' due to its role as a center of enlightenment during the Age of Enlightenment and its early adoption of street lighting.",
+    budget: [232,242],
+    img:"/src/assets/images/cities/paris.avif"
+  },
+  {
+    id: 3,
+    name: "Marrakech",
+    description: "Marrakech, the vibrant heart of Morocco, is a tapestry of colors, scents, and sounds that awaken the senses. The ancient medina is a maze of narrow alleyways brimming with bustling souks, where artisans weave intricate carpets and sell spices that perfume the air with exotic fragrances. Majestic palaces and ornate mosques showcase the city's rich history and architectural splendor, while tranquil riads offer a peaceful oasis amidst the lively bustle.",
+    budget: [63,68],
+    img:"/src/assets/images/cities/marrakech.avif"
+  },
+  {
+    id: 4,
+    name: "Amsterdam",
+    description: "Amsterdam, the capital city of the Netherlands, is a vibrant and eclectic metropolis known for its picturesque canals, historic architecture, and diverse cultural scene. With its iconic narrow houses, lively neighborhoods, and world-class museums such as the Van Gogh Museum and the Anne Frank House, Amsterdam offers a wealth of experiences for visitors. The city's relaxed atmosphere and progressive attitude make it a popular destination for travelers seeking both history and modernity.",
+    budget: [175,187],
+    img:"/src/assets/images/cities/amsterdam.avif"
+  },
+  {
+    id: 5,
+    name: "Dakar",
+    description: "Dakar is the vibrant capital city of Senegal, situated on the westernmost point of Africa. Known for its bustling markets, colorful culture, and lively music scene, Dakar offers visitors a unique blend of traditional African charm and modern urban energy.",
+    budget: [57,61],
+    img:"/src/assets/images/cities/dakar.avif"
+  },
+  {
+    id: 6,
+    name: "Cairo",
+    description: "Cairo, the vibrant capital of Egypt, is an intoxicating blend of past and present. Dominated by the majestic pyramids of Giza, it is filled with historical treasures such as the Egyptian Museum, home to countless Pharaonic artifacts. The bustling streets of the city resonate with the constant buzz of everyday life, while colorful markets offer an explosion of flavors and souvenirs. Towering minarets rise above the urban landscape, reminding of Egypt's rich culture and spirituality.",
+    budget: [16,17],
+    img:"/src/assets/images/cities/cairo.avif"
+  },
+  {
+    id: 7,
+    name: "Rome",
+    description: "Rome, the capital city of Italy, is a place where ancient history and modern life blend seamlessly. It's renowned for its rich history, breathtaking architecture, and vibrant culture. From iconic landmarks like the Colosseum and the Roman Forum to the Vatican City with St. Peter's Basilica, Rome offers a treasure trove of historical sites and artistic masterpieces. With its charming piazzas, cobblestone streets, and delectable cuisine, Rome is a destination that captivates visitors from around the globe.",
+    budget: [170,182],
+    img:"/src/assets/images/cities/rome.avif"
+  },
+  {
+    id: 8,
+    name: "Marbella",
+    description: "Marbella, a gem on Spain's Costa del Sol, dazzles with its golden sand beaches bathed by the crystal-clear waters of the Mediterranean. This cosmopolitan city combines the elegance of its historic center with luxurious marinas dotted with sparkling yachts. Between its picturesque alleys, designer boutiques, and gourmet restaurants, Marbella offers an enchanting blend of culture, glamour, and relaxation.",
+    budget: [117,125],
+    img:"/src/assets/images/cities/marbella.avif"
+  },
+  {
+    id: 9,
+    name: "Copenhagen",
+    description: "Copenhagen, the capital city of Denmark, is a dynamic and cosmopolitan metropolis renowned for its blend of historic charm and modern innovation. Situated on the eastern coast of the island of Zealand, Copenhagen is characterized by its picturesque waterfront, colorful buildings, and historic landmarks. The city is home to iconic attractions such as the 17th-century Nyhavn harbor, the enchanting Tivoli Gardens amusement park, and the world-famous Little Mermaid statue. Known for its commitment to sustainability and quality of life, Copenhagen is also celebrated for its thriving culinary scene, innovative design, and cycling culture. With its welcoming atmosphere and progressive spirit, Copenhagen offers visitors a unique blend of history, culture, and contemporary flair.",
+    budget: [196,210],
+    img:"/src/assets/images/cities/copenhag.avif"
+  },
+  {
+    id: 10,
+    name: "Bali",
+    description: "Bali, known as the Island of the Gods, captivates with its breathtaking landscapes, from lush rice terraces to pristine beaches kissed by turquoise waters. This Indonesian paradise exudes a mystical charm, with ornate temples nestled amidst verdant jungles and traditional villages alive with vibrant culture. Immerse yourself in the rhythmic beat of Balinese gamelan music, indulge in mouthwatering cuisine, or simply relax in a tranquil yoga retreat overlooking serene rice paddies.",
+    budget: [69,74],
+    img:"/src/assets/images/cities/bali.avif"
+  }
+  
+]
+
+export default destinations;

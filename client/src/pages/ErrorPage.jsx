d5ee78839367370bf0bf56dc2f57ba48d6beedd6
Naïm <naimoudinesaidi@gmail.com>
2024-05-13 10:49:05 +0200

    dernière modif non push

--- client/src/pages/ErrorPage.jsx
+++ client/src/pages/ErrorPage.jsx
@@ -1,11 +1,10 @@
-import { Link } from "react-router-dom"
+import { Link } from "react-router-dom";
 
 export default function ErrorPage() {
-    return(
-        <>
-            <h1>Error 404</h1>
-            <Link to='/'>go back to home page</Link>
-        </>
-
-    )
+  return (
+    <>
+      <h1>Error 404</h1>
+      <Link to="/">go back to home page</Link>
+    </>
+  );
 }

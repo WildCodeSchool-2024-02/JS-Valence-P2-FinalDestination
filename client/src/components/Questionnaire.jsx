b3e5d3f93e2a64dc0109726b53a6e94faef39dd3
SMAIL ALAHYANE <sm.alahyane@gmail.com>
2024-05-14 17:34:30 +0200

    Questionnaire style

--- client/src/components/Questionnaire.jsx
+++ client/src/components/Questionnaire.jsx
@@ -1,8 +1,6 @@
 import { useState } from "react";
 import "./Questionnaire.css";
 import Modal from "./Modal";
-import womanPhoto from "../assets/images/woman.png";
-import airPlanePhoto from "../assets/images/airplane.png";
 
 function Questionnaire() {
   const [questions] = useState([
@@ -54,46 +52,37 @@ function Questionnaire() {
 
   return (
     <div>
-      <h1>To see our suggestions, please answer all the questions below :</h1>
-      <div className="main-container">
-        <div className="airplane-and-woman">
-          <img className="airplane" src={airPlanePhoto} alt="airplane" />
-          <img
-            className="traveller-woman"
-            src={womanPhoto}
-            alt="traveller-woman"
-          />
+      <div className="questions-and-button">
+        <div className="questionnaire">
+          <h1>To see our suggestions, please answer all the questions :</h1>
+          {questions.map((question) => (
+            <div key={question.id} className="question">
+              <h2>{question.question}</h2>
+              {question.responses.map((response) => (
+                <label key={response}>
+                  <input
+                    type="radio"
+                    name={`question-${question.id}`}
+                    value={response}
+                    onChange={() => handleResponseChange(question.id, response)}
+                    checked={answers[question.id] === response}
+                  />
+                  {response}
+                </label>
+              ))}
+            </div>
+          ))}
+          <button className="Action" type="submit" onClick={handleSubmit}>
+            Continue
+          </button>
         </div>
-        <div className="questions-and-button">
-          <div className="questionnaire">
-            {questions.map((question) => (
-              <div key={question.id} className="question">
-                <h2>{question.question}</h2>
-                {question.responses.map((response) => (
-                  <label key={response}>
-                    <input
-                      type="radio"
-                      name={`question-${question.id}`}
-                      value={response}
-                      onChange={() =>
-                        handleResponseChange(question.id, response)
-                      }
-                      checked={answers[question.id] === response}
-                    />
-                    {response}
-                  </label>
-                ))}
-              </div>
-            ))}
-            <button className="Action" type="submit" onClick={handleSubmit}>
-              Continue
-            </button>
-          </div>
 
-          <Modal show={showModal} onClose={handleClose}>
-            <h2 className="PopupTitle">Would you like to be informed</h2>
-            <h3>about our new plans</h3>
-            <p className="PopupCall">Please fill in to continue</p>
+        <Modal show={showModal} onClose={handleClose}>
+          <div className="PopUp">
+            <h2 className="PopupTitle">Would you like to be informed?</h2>
+            <h3>
+              about our new plans <br /> please fill in to continue
+            </h3>
             <form onSubmit={handleFormSubmit}>
               <input className="Name" type="text" placeholder="Name" required />
               <input
@@ -102,12 +91,12 @@ function Questionnaire() {
                 placeholder="E-mail"
                 required
               />
-              <button className="Submit" type="submit">
+              <button className="SubmitInfo" type="submit">
                 Continue
               </button>
             </form>
-          </Modal>
-        </div>
+          </div>
+        </Modal>
       </div>
     </div>
   );

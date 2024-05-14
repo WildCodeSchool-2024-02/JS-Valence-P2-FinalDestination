da512aa1b51199498267f4edcfed5ae939376d61
BENSFIA <mobensfia@gmail.com>
2024-05-14 10:16:31 +0200

    ajout airplane + emplacement des images sur page de destination

--- client/src/pages/DestinationsPage.jsx
+++ client/src/pages/DestinationsPage.jsx
@@ -1,3 +1,52 @@
+import airplane from "../assets/images/AVIF photos/airplane.png";
+
+import "./DestinationPage.css";
+
 export default function DestinationsPage() {
-  return <h1>Hello from Destination Page</h1>;
+  return (
+    <>
+      <div className="text">
+        <img className="airplaine" src={airplane} alt="airplane" />
+
+        <p>
+          the most visited <br />
+          holiday routes for you
+        </p>
+        <h3>We have brought together</h3>
+      </div>
+
+      <div className="contenu">
+        <div className="picture">
+          <h2>amsterdam</h2>
+        </div>
+        <div className="picture">
+          <h2>amsterdam</h2>
+        </div>
+        <div className="picture">
+          <h2>amsterdam</h2>
+        </div>
+        <div className="picture">
+          <h2>amsterdam</h2>
+        </div>
+        <div className="picture">
+          <h2>amsterdam</h2>
+        </div>
+        <div className="picture">
+          <h2>amsterdam</h2>
+        </div>
+        <div className="picture">
+          <h2>amsterdam</h2>
+        </div>
+        <div className="picture">
+          <h2>amsterdam</h2>
+        </div>
+        <div className="picture">
+          <h2>amsterdam</h2>
+        </div>
+        <div className="picture">
+          <h2>amsterdam</h2>
+        </div>
+      </div>
+    </>
+  );
 }

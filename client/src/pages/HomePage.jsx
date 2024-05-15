45b38918e1e4d579057263183c7584382df1d8d5
derimer <jderimer@gmail.com>
2024-05-15 11:15:14 +0200

    elaboration de la page Home_page

--- client/src/pages/HomePage.jsx
+++ client/src/pages/HomePage.jsx
@@ -1,11 +1,40 @@
-export default function HomePage() {
+import femaleAbuSimbel from "../assets/images/female-abu-simbel-temple-southern-egypt-lake-nasser.avif";
+import Lettering from "../assets/images/lettering.png";
+import "./Home.css";
 
-	return (
-		<>
-			<h1>Home page</h1>
-			<div>
-				<article><h2>Je suis un article</h2></article>
-			</div>
-		</>
-	)
+export default function HomePage() {
+  return (
+    <div className="Homepage">
+      <div className="Vaccation">
+        <img src={Lettering} alt="" className="Lettering" />
+        <br />
+        <p>
+          Vaccation is approaching,
+          <br />
+          but you still haven't decided
+        </p>
+
+        <span className="Where">"WHERE TO GO?"</span>
+
+        <br />
+        <br />
+        <h2>
+          Be ready
+          <br />
+          to know !<br />
+        </h2>
+        <button type="submit">START QUIZ</button>
+      </div>
+
+      <h3>
+        The most preferred
+        <br />
+        <br /> cultural route of 2023
+        <br />
+        <br />
+        <span className="Caire">CAIRE</span>
+      </h3>
+      <img src={femaleAbuSimbel} alt="abu-simbel" className="femaleAbuSimbel" />
+    </div>
+  );
 }

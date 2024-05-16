3c5b1360125722cd5a74a7982b166f86e18986b5
BENSFIA <mobensfia@gmail.com>
2024-05-16 09:59:54 +0200

    first commit

--- client/src/pages/DestinationsPage.jsx
+++ client/src/pages/DestinationsPage.jsx
@@ -1,52 +1,30 @@
-import airplane from "../assets/images/airplane.jpg";
+import db from "../db";
 
 import "./DestinationPage.css";
 
 export default function DestinationsPage() {
   return (
-    <>
-      <div className="plane">
+    <div className="page_destination">
+      {/* <div className="plane">
         <img className="airplaine" src={airplane} alt="airplane" />
-      </div>
-
-      <p className="paragraphe">We have brought together</p>
-
+      </div> */}
       <div className="title">
-        <h1>the most visited </h1>
+        <h1 className="paragraphe">
+          We have brought together<span>the most visited</span>
+        </h1>
         <h2> holiday routes for you!</h2>
       </div>
-      <div className="container">
-        <div className="picture">
-          <h3>Amsterdam</h3>
-        </div>
-        <div className="picture">
-          <h3>Amsterdam</h3>
-        </div>
-        <div className="picture">
-          <h3>Amsterdam</h3>
-        </div>
-        <div className="picture">
-          <h3>Amsterdam</h3>
-        </div>
-        <div className="picture">
-          <h3>Amsterdam</h3>
-        </div>
-        <div className="picture">
-          <h3>Amsterdam</h3>
-        </div>
-        <div className="picture">
-          <h3>Amsterdam</h3>
-        </div>
-        <div className="picture">
-          <h3>Amsterdam</h3>
-        </div>
-        <div className="picture">
-          <h3>Amsterdam</h3>
-        </div>
-        <div className="picture">
-          <h3>Amsterdam</h3>
-        </div>
+      <div className="card_container">
+        {db.map((element) => (
+          <div
+            key={element.id}
+            className="picture"
+            style={{ background: `url(${element.img}) center/cover` }}
+          >
+            <h3>{element.name}</h3>
+          </div>
+        ))}
       </div>
-    </>
+    </div>
   );
 }

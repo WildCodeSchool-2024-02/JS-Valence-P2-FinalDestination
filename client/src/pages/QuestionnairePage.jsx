a743d2dc7b1041e5a0e57eb31a1eff2b17f63094
SMAIL ALAHYANE <sm.alahyane@gmail.com>
2024-05-21 15:21:40 +0200

    Mettre en place le filtre de la recherche des suggestions

--- client/src/pages/QuestionnairePage.jsx
+++ client/src/pages/QuestionnairePage.jsx
@@ -1,53 +1,118 @@
-import { useState } from "react";
+import { useState, useEffect } from "react";
+import { useNavigate, useOutletContext } from "react-router-dom";
 import "./QuestionnairePage.css";
 import Modal from "../components/Modal";
+import destinations from "../db";
 
-function Questionnaire() {
+function QuestionnairePage() {
   const [questions] = useState([
     {
       id: "Type",
-      question: "What type of activities do you plan to do during your stay ?",
+      question: "What type of activities do you plan to do during your stay?",
       responses: ["Fun/Relaxation", "Cultural", "Gastronomy", "Nature"],
     },
     {
       id: "Budget",
-      question: "What’s your daily budget ?",
-      responses: ["20$ - 50$", "50$ - 100$", "100$ - 200$", "+200$"],
+      question: "What’s your daily budget?",
+      responses: ["20$ - 50$", "50$ - 100$", "100$ - 300$", "+300$"],
     },
     {
       id: "Season",
-      question: "Which season do you prefer ?",
+      question: "Which season do you prefer?",
       responses: ["Winter", "Autumn", "Spring", "Summer"],
     },
     {
       id: "Vehicle",
-      question: "You’ll need to rent a vehicle ?",
+      question: "You’ll need to rent a vehicle?",
       responses: ["Yes", "No"],
     },
     {
       id: "Group",
-      question: "How many people are you ?",
+      question: "How many people are you?",
       responses: ["Alone", "Couple", "More than 2"],
     },
   ]);
 
+  const [setFormData] = useOutletContext();
   const [answers, setAnswers] = useState({});
   const [showModal, setShowModal] = useState(false);
+  const [errorMessage, setErrorMessage] = useState("");
+  const navigate = useNavigate();
 
-  const handleResponseChange = (questionId, response) => {
-    setAnswers({ ...answers, [questionId]: response });
+  useEffect(() => {}, [answers]);
+
+  const handleResponseChange = (event, questionId) => {
+    setAnswers((oldAnswers) => ({
+      ...oldAnswers,
+      [questionId]: event.target.value,
+    }));
   };
 
-  const handleSubmit = () => {
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    const selectedAnswersCount = Object.keys(answers).length;
+    if (selectedAnswersCount < 2) {
+      setErrorMessage("Please select at least two criteria.");
+      return;
+    }
+    setErrorMessage("");
     setShowModal(true);
+
+    setFormData(answers);
   };
+
   const handleClose = () => {
     setShowModal(false);
   };
+
   const handleFormSubmit = (e) => {
     e.preventDefault();
+    // eslint-disable-next-line no-use-before-define
+    const filteredDestinations = filterDestinations(answers);
+    navigate("/suggestions", { state: { suggestions: filteredDestinations } });
     handleClose();
   };
+
+  // eslint-disable-next-line no-shadow
+  const filterDestinations = (answers) =>
+    destinations.filter((destination) => {
+      const budgetRange = answers.Budget?.split(" ")[0].replace("$", "");
+      const budgetLower = parseInt(budgetRange?.split("-")[0], 10);
+      const budgetUpper = parseInt(
+        budgetRange?.split("-")[1] || budgetLower,
+        10
+      );
+
+      const matchesBudget =
+        budgetRange &&
+        destination.budget[0] <= budgetUpper &&
+        destination.budget[1] >= budgetLower;
+      const matchesActivities = answers.Type
+        ? destination.activities.includes(answers.Type.toLowerCase())
+        : true;
+      const matchesSeason = answers.Season
+        ? destination.activities.includes(answers.Season.toLowerCase())
+        : true;
+      const matchesVehicle = answers.Vehicle
+        ? destination.activities.includes(answers.Vehicle.toLowerCase())
+        : true;
+      const matchesGroup = answers.Group
+        ? destination.activities.includes(answers.Group.toLowerCase())
+        : true;
+
+      return (
+        (matchesBudget && matchesActivities) ||
+        (matchesBudget && matchesSeason) ||
+        (matchesBudget && matchesVehicle) ||
+        (matchesBudget && matchesGroup) ||
+        (matchesActivities && matchesSeason) ||
+        (matchesActivities && matchesVehicle) ||
+        (matchesActivities && matchesGroup) ||
+        (matchesSeason && matchesVehicle) ||
+        (matchesSeason && matchesGroup) ||
+        (matchesVehicle && matchesGroup)
+      );
+    });
 
   return (
     <div className="PageQuestionnaire">
@@ -63,50 +128,47 @@ function Questionnaire() {
                     type="radio"
                     name={`question-${question.id}`}
                     value={response}
-                    onChange={() => handleResponseChange(question.id, response)}
+                    onChange={(event) =>
+                      handleResponseChange(event, question.id)
+                    }
                     checked={answers[question.id] === response}
                     style={{ marginRight: "8px" }}
+                    required
                   />
                   {response}
                 </label>
               ))}
             </div>
           ))}
+          {errorMessage && <p style={{ color: "red" }}>{errorMessage}</p>}
           <button className="Action" type="submit" onClick={handleSubmit}>
             Continue
           </button>
         </div>
 
-        {showModal && (
-          <Modal show={showModal} onClose={handleClose}>
-            <div className="PopUp">
-              <h2 className="PopupTitle">Would you like to be informed?</h2>
-              <h3>
-                about our new plans <br /> please fill in to continue
-              </h3>
-              <form onSubmit={handleFormSubmit}>
-                <input
-                  className="Name"
-                  type="text"
-                  placeholder="Name"
-                  required
-                />
-                <input
-                  className="Email"
-                  type="email"
-                  placeholder="E-mail"
-                  required
-                />
-                <button className="SubmitInfo" type="submit">
-                  Continue
-                </button>
-              </form>
-            </div>
-          </Modal>
-        )}
+        <Modal show={showModal} onClose={handleClose}>
+          <div className="PopUp">
+            <h2 className="PopupTitle">Would you like to be informed?</h2>
+            <h3>
+              about our new plans <br /> please fill in to continue
+            </h3>
+            <form onSubmit={handleFormSubmit}>
+              <input className="Name" type="text" placeholder="Name" required />
+              <input
+                className="Email"
+                type="email"
+                placeholder="E-mail"
+                required
+              />
+              <button className="SubmitInfo" type="submit">
+                Continue
+              </button>
+            </form>
+          </div>
+        </Modal>
       </div>
     </div>
   );
 }
 
-export default Questionnaire;
+export default QuestionnairePage;
--- client/src/pages/SuggestionsPage.jsx
+++ client/src/pages/SuggestionsPage.jsx
@@ -1,7 +1,10 @@
+// import { useOutletContext } from "react-router-dom";
 import SuggestionCard from "../components/SuggestionCard";
 import styles from "./SuggestionsPage.module.css";
 
 export default function SuggestionPage() {
+  // const [formData, setFormData] = useOutletContext();
+
   return (
     <div className={styles.mainSuggestion}>
       <h1>Here are, our suggestions for you!</h1>

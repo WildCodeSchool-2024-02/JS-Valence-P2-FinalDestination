726aabf103b983de63726ec9be57915aceccfccc
iremchabanne <irem_kcakir@outlook.com>
2024-05-21 10:12:46 +0200

    fixed h1 to the center and removed the background image for middle size

--- client/src/pages/SuggestionsPage.jsx
+++ client/src/pages/SuggestionsPage.jsx
@@ -4,9 +4,7 @@ import styles from "./SuggestionsPage.module.css";
 export default function SuggestionPage() {
   return (
     <div className={styles.mainSuggestion}>
-      <h1>
-        Here are, <br /> our suggestions for you!
-      </h1>
+      <h1>Here are, our suggestions for you!</h1>
       <div className={styles.cardContainer}>
         <SuggestionCard />
         <SuggestionCard />

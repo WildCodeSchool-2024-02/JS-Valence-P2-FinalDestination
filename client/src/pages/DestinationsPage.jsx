72b3687e229317280f694e9b18188161d6d5852b
BENSFIA <mobensfia@gmail.com>
2024-05-21 14:35:29 +0200

    augmentation taille des images de villes + ajout hover sur les noms de villes

--- client/src/pages/DestinationsPage.jsx
+++ client/src/pages/DestinationsPage.jsx
@@ -1,12 +1,11 @@
+import { useNavigate } from "react-router-dom";
 import db from "../db";
 import "./DestinationPage.css";
 
-export default function DestinationsPage() {
+export default function DestinationPage() {
+  const navigate = useNavigate();
   return (
     <div className="page_destination">
-      {/* <div className="plane">
-        <img className="airplaine" src={airplane} alt="airplane" />
-      </div> */}
       <div className="title">
         <h1 className="paragraphe">
           We have brought together<span> the most visited</span>
@@ -18,7 +17,9 @@ export default function DestinationsPage() {
           <div
             key={element.id}
             className="picture"
+            onClick={() => navigate(`/destination/${element.id}`)}
             style={{ background: `url(${element.img}) center/cover` }}
+            role="presentation"
           >
             <h3>{element.name}</h3>
           </div>

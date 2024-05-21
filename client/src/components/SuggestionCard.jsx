16106110d9934866fe08777311da0578976a9b19
iremchabanne <irem_kcakir@outlook.com>
2024-05-21 14:28:50 +0200

    The suggestions were diversified.

--- client/src/components/SuggestionCard.jsx
+++ client/src/components/SuggestionCard.jsx
@@ -1,11 +1,11 @@
 import styles from "./SuggestionCard.module.css";
-import rome from "../assets/images/cities/rome.avif";
 
-export default function SuggestionCard() {
+export default function SuggestionCard(description) {
+  const { img, name } = description;
   return (
     <div className={styles.card}>
-      <img className={styles.suggestionImg} src={rome} alt="rome" />
-      <h2>ROME</h2>
+      <img className={styles.suggestionImg} src={img} alt={name} />
+      <h2>{name}</h2>
       <button className={styles.suggestionCardButton} type="button">
         Detail
       </button>

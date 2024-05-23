be40b803cea831b7038444405b20e3881ac54c60
iremchabanne <irem_kcakir@outlook.com>
2024-05-23 09:59:25 +0200

    Connection between answers and suggestions. Additions for no suggestion situation.

--- client/src/components/SuggestionCard.jsx
+++ client/src/components/SuggestionCard.jsx
@@ -1,14 +1,15 @@
+import { Link } from "react-router-dom";
 import PropTypes from "prop-types";
 import styles from "./SuggestionCard.module.css";
 
-function SuggestionCard({ img, name }) {
+function SuggestionCard({ img, name, id }) {
   return (
     <div className={styles.card}>
       <img className={styles.suggestionImg} src={img} alt={name} />
       <h2>{name}</h2>
-      <button className={styles.suggestionCardButton} type="button">
-        Detail
-      </button>
+      <Link to={`/destination/${id}`} className={styles.suggestionCardLink}>
+        Details
+      </Link>
     </div>
   );
 }
@@ -16,6 +17,7 @@ function SuggestionCard({ img, name }) {
 SuggestionCard.propTypes = {
   img: PropTypes.string.isRequired,
   name: PropTypes.string.isRequired,
+  id: PropTypes.number.isRequired,
 };
 
 export default SuggestionCard;
